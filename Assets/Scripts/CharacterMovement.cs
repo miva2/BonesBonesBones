a114575b4c809c7c45e1fe2f209f781780d2d0fa
miva2 <miva2-_-@hotmail.com>
2020-01-31 21:58:58 +0000

    Very basic character movement script

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    public Vector3 targetPosition;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * 5);
+    }
+}
