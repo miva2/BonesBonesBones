37dd275f99b92f5cb2bbd35cafe60a72fae4aed7
miva2 <miva2-_-@hotmail.com>
2020-01-31 22:22:34 +0000

    Disabled character movement. The second solution is probably better

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
 
-    public Vector3 targetPosition;
+    private Vector3 targetPosition;
+
+    //public LayerMask groundLayerMask; -- from video --> use collider instead
 
 
 
@@ -18,12 +20,31 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log("TargetPosition = " + targetPosition);
-        }
 
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * 5);
+        // first method: This doesnt work. ScreenToWorldPoint() is not a magic method
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //    Debug.Log("TargetPosition = " + targetPosition);
+        //}
+
+        //transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * 5);
+
+
+
+        // second method
+
+        //Ray mousePointerRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //RaycastHit hitInfo;
+
+        //if(Physics.Raycast (mousePointerRay, out hitInfo, 100, groundLayerMask))
+        //{
+        //targetPosition = hitInfo.transform.position;
+        // targetPosition is the location where the ray hits the ground collider
+
+        //}
+
+        //transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * 5);
     }
 }
