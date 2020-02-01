05d3468a700024778c8186ad57086510335d9488
miva2 <miva2-_-@hotmail.com>
2020-02-01 10:54:17 +0000

    Added skeleton model

---
+++ Assets/Scripts/SimplePatrol.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePatrol : MonoBehaviour
+{
+
+    public GameObject[] patrolPoints;
+
+    private Vector3 nextDestination;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextDestination = patrolPoints[0].transform.position;
+    }
+
+// Update is called once per frame
+void Update()
+    {
+        //transform.position = Vector3.MoveTowards
+    }
+}
