10fd7cc31ed64946e233ef39c6b98c714ac5c3f6
Jesse Gielen <jessegielen@hotmail.com>
2020-02-01 19:05:10 +0000

    Move Bone enums into Bone.cs

--- Assets/Scripts/Bone.cs
+++ Assets/Scripts/Bone.cs
@@ -1,4 +1,22 @@
+using System;
 using UnityEngine;
+
+public enum JointType
+{
+    One,
+    Two,
+    Three
+}
+
+[Flags]
+public enum BoneType
+{
+    Head = 0,
+    LeftUpperArm = 1 << 0,
+    LeftLowerArm = 1 << 1,
+    RightUpperArm = 1 << 2,
+    RightLowerArm = 1 << 3
+}
 
 public class Bone : MonoBehaviour
 {
--- Assets/Scripts/Helpers/JointType.cs
+++
@@ -1,6 +0,0 @@
-public enum JointType
-{
-    One,
-    Two,
-    Three
-}
