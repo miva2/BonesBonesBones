cc39835bae7f2889fb6ad627f56004d785ef5f71
Roman Prosimír Vaňo <vano.roman.sk@gmail.com>
2020-02-01 15:05:47 +0000

    Indicator movement INIT

--- Assets/Scripts/IndicatorLogic.cs
+++ Assets/Scripts/IndicatorLogic.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class IndicatorLogic : MonoBehaviour
 {
+        /// <summary>BattleUI GameObject;</summary>
+        private GameObject father;
+        private float indicatorMoveSpeed = 0.3f;
+        [SerializeField]
+        private Vector3 moveVector;
+
+    private void Start()
+    {
+        father = this.GetComponentInParent<Canvas>().gameObject;
+    }
+    private void Update()
+    {
+        Indicator_AutoMove();
+    }
+
     public void Attack()
     {
         print("HIT!");
+    }
+
+    private void Indicator_AutoMove(){
+        if (father.activeInHierarchy == true)
+        {
+            while(true){
+                transform.Translate(moveVector * indicatorMoveSpeed * Time.deltaTime);
+            }
+        }
     }
 }
