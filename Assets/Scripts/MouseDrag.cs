723692e18bfe5a8daf52e168839864fa0146f654
Jesse Gielen <jessegielen@hotmail.com>
2020-02-01 01:11:35 +0000

    WIP repair UI (I can't figure out drag and drop)

---
+++ Assets/Scripts/MouseDrag.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MouseDrag : MonoBehaviour
+{
+    Vector3 dragOffset;
+    bool dragging;
+    Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dragging)
+        {
+            DoDrag();
+        }
+        else
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+    }
+
+    void DoDrag()
+    {
+        transform.position = Input.mousePosition + dragOffset;
+    }
+}
