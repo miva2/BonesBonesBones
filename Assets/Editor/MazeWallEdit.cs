4523252973db14ec778915e0d5acad287166f0a0
Jesse Gielen <jessegielen@hotmail.com>
2020-02-01 23:53:26 +0000

    WIP maze editor + import drag frame and ceiling sprites

---
+++ Assets/Editor/MazeWallEdit.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+[InitializeOnLoad]
+[CustomEditor(typeof(MazeWallEditor), true)]
+[CanEditMultipleObjects]
+public class MazeWallEdit : Editor
+{
+    public int gridSize = 5;
+
+    private void OnSceneGUI()
+    {
+        var e = Event.current;
+
+        if (e.type == EventType.KeyDown)
+        {
+            HandleKeyPress(e.keyCode);
+        }
+    }
+
+    private void HandleKeyPress(KeyCode keyCode)
+    {
+        var transforms = Selection.transforms;
+        Action<Transform> selectionAction = GetSelectedKeyAction(keyCode);
+        Action otherAction = GetOtherKeyAction(keyCode);
+
+        if (selectionAction == null && otherAction == null) return;
+
+        // absorb the event
+        Event.current.Use();
+
+        if (selectionAction != null)
+        {
+            foreach (var t in transforms)
+            {
+                selectionAction(t);
+            }
+        }
+
+        otherAction?.Invoke();
+    }
+
+    private Action<Transform> GetSelectedKeyAction(KeyCode keyCode)
+    {
+        switch (keyCode)
+        {
+            case KeyCode.F:
+                return t =>
+                {
+                    var angles = t.rotation.eulerAngles;
+                    angles.x *= -1;
+                    t.rotation = Quaternion.Euler(angles);
+                };
+            case KeyCode.LeftArrow:
+                return t => t.Translate(-gridSize, 0, 0);
+            case KeyCode.RightArrow:
+                return t => t.Translate(gridSize, 0, 0);
+            case KeyCode.UpArrow:
+                return t => t.Translate(0, -gridSize, 0);
+            case KeyCode.DownArrow:
+                return t => t.Translate(0, gridSize, 0);
+            case KeyCode.D:
+                return t =>
+                {
+                    Instantiate<GameObject>(t.gameObject);
+                };
+        }
+
+        return null;
+    }
+
+    private Action<Transform> GetAllSelectedKeyAction(KeyCode keyCode)
+    {
+        switch (keyCode)
+        {
+            case KeyCode.F:
+                return t =>
+                {
+                    var angles = t.rotation.eulerAngles;
+                    angles.x *= -1;
+                    t.rotation = Quaternion.Euler(angles);
+                };
+            case KeyCode.LeftArrow:
+                return t => t.Translate(-gridSize, 0, 0);
+            case KeyCode.RightArrow:
+                return t => t.Translate(gridSize, 0, 0);
+            case KeyCode.UpArrow:
+                return t => t.Translate(0, -gridSize, 0);
+            case KeyCode.DownArrow:
+                return t => t.Translate(0, gridSize, 0);
+            case KeyCode.D:
+                return t =>
+                {
+                    Instantiate<GameObject>(t.gameObject);
+                };
+        }
+
+        return null;
+    }
+
+    private Action GetOtherKeyAction(KeyCode keyCode)
+    {
+        switch (keyCode)
+        {
+        }
+
+        return null;
+    }
+
+}
---
+++ Assets/MazeWallEditor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeWallEditor : MonoBehaviour
+{
+#if UNITY_EDITOR
+
+    public bool snapToGrid = true;
+    public float snapValue = 5f;
+
+#endif
+}
