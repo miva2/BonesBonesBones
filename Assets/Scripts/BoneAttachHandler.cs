0178ea5ac0852ee5e0b4d6ec8f76730760ab2456
Jesse Gielen <jessegielen@hotmail.com>
2020-02-01 10:40:51 +0000

    Check UI bone overlap

---
+++ Assets/Scripts/BoneAttachHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneAttachHandler : MonoBehaviour
+{
+    public GameObject otherBone;
+
+    RectTransform rectTransform;
+    RectTransform otherRectTransform;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        otherRectTransform = otherBone?.GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if (otherRectTransform != null && BonesOverlap())
+        {
+            AttachBones();
+        }
+    }
+
+    private bool BonesOverlap()
+    {
+        var first = RectTransformToScreenSpace(rectTransform);
+        var second = RectTransformToScreenSpace(otherRectTransform);
+        return first.Overlaps(second);
+    }
+
+    private void AttachBones()
+    {
+        Debug.Log("Bones overlap.");
+    }
+
+    static Rect RectTransformToScreenSpace(RectTransform transform)
+    {
+        Vector2 size = Vector2.Scale(transform.rect.size, transform.lossyScale);
+        return new Rect((Vector2)transform.position - (size * 0.5f), size);
+    }
+}
