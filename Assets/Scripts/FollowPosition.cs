44e416194d313358236abf50e453725c28137197
Jesse Gielen <jessegielen@hotmail.com>
2020-02-01 22:58:15 +0000

    Lighting and better wall texturing

---
+++ Assets/Scripts/FollowPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPosition : MonoBehaviour
+{
+    public Transform followTarget;
+
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - followTarget.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = followTarget.position + offset;
+    }
+}
---
+++ Assets/Scripts/TextureScaler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureScaler : MonoBehaviour
+{
+    Material material;
+
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+    }
+
+    void Update()
+    {
+        var s = transform.localScale;
+        material.mainTextureScale = new Vector2(s.x, s.y);
+    }
+}
