9a59e4ad27e1361ce54c41e818e93b4ada53bb12
Jesse Gielen <jessegielen@hotmail.com>
2020-02-01 18:36:58 +0000

    Code for taking hits

--- Assets/Scripts/BonyCharacter.cs
+++ Assets/Scripts/BonyCharacter.cs
@@ -18,14 +18,51 @@ public class BonyCharacter : MonoBehaviour
     {
     }
 
+    public void Hit(BoneType type)
+    {
+        Debug.Log($"Hit bone of type {type}.");
+
+        var health = GetHealth();
+        if (health == 1 && type == BoneType.Head)
+        {
+            headHasBeenHit = true;
+            Die();
+        }
+
+        if (type != BoneType.Head)
+        {
+            // mask out the bonetype that was hit
+            bones &= ~type;
+            ThrowOutBone(type);
+        }
+        else
+        {
+            Stun();
+        }
+    }
+
+    private void ThrowOutBone(BoneType type)
+    {
+    }
+
+    private void Die()
+    {
+        Debug.Log("Character died.");
+    }
+
+    private void Stun()
+    {
+        Debug.Log("Stunned the enemy.");
+    }
+
     public int GetHealth()
     {
         var health = 0;
+        if (!headHasBeenHit) health++;
         if (HasBone(BoneType.LeftLowerArm)) health++;
         if (HasBone(BoneType.LeftUpperArm)) health++;
         if (HasBone(BoneType.RightLowerArm)) health++;
         if (HasBone(BoneType.RightUpperArm)) health++;
-        if (!headHasBeenHit) health++;
         return health;
     }
 
--- Assets/Scripts/Helpers/BoneType.cs
+++ Assets/Scripts/Helpers/BoneType.cs
@@ -3,6 +3,7 @@ using System;
 [Flags]
 public enum BoneType
 {
+    Head = 0,
     LeftUpperArm = 1 << 0,
     LeftLowerArm = 1 << 1,
     RightUpperArm = 1 << 2,
