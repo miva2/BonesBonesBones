289fd69cf56be99af1edd9bbfdd6257f92450e52
miva2 <miva2-_-@hotmail.com>
2020-02-01 11:44:24 +0000

    Implemented simple patrol script. The entity moves to the given positions, looping through all positions.

--- Assets/Scripts/SimplePatrol.cs
+++ Assets/Scripts/SimplePatrol.cs
@@ -6,19 +6,48 @@ public class SimplePatrol : MonoBehaviour
 {
 
     public GameObject[] patrolPoints;
+    public float moveSpeed;
+
 
     private Vector3 nextDestination;
+    private int patrolPointIndex = 0;
+    private bool destinationReached = false;
+
+    private float destinationReachedRadius = 0.1f;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        nextDestination = patrolPoints[0].transform.position;
+        nextDestination = patrolPoints[patrolPointIndex].transform.position;
     }
 
-// Update is called once per frame
-void Update()
+    // Update is called once per frame
+    void Update()
     {
-        //transform.position = Vector3.MoveTowards
+        transform.position = Vector3.MoveTowards(transform.position, nextDestination, moveSpeed);
+
+        destinationReached = Vector3.Distance(transform.position, nextDestination) <= destinationReachedRadius;
+
+        if (destinationReached)
+        {
+            UpdatePatrolPointIndex();
+            nextDestination = patrolPoints[patrolPointIndex].transform.position;
+        }
+    }
+
+    void UpdatePatrolPointIndex()
+    {
+        int amount = patrolPoints.Length;
+
+        if(patrolPointIndex <= amount)
+        {
+            patrolPointIndex++;
+        }
+        else
+        {
+           patrolPointIndex = 0;
+        }
     }
 }
