3dbfcdbeca31f5764f51fbab044e52f201bcd65d
Jesse Gielen <jessegielen@hotmail.com>
2020-02-02 00:18:15 +0000

    We now have a maze editor

--- Assets/Editor/MazeWallEdit.cs
+++ Assets/Editor/MazeWallEdit.cs
@@ -43,6 +43,8 @@ public class MazeWallEdit : Editor
 
     private Action<Transform> GetSelectedKeyAction(KeyCode keyCode)
     {
+        var sceneCamera = SceneView.lastActiveSceneView.camera;
+
         switch (keyCode)
         {
             case KeyCode.F:
@@ -50,49 +52,61 @@ public class MazeWallEdit : Editor
                 {
                     var angles = t.rotation.eulerAngles;
                     angles.x *= -1;
+                    angles.z *= -1;
                     t.rotation = Quaternion.Euler(angles);
                 };
-            case KeyCode.LeftArrow:
-                return t => t.Translate(-gridSize, 0, 0);
-            case KeyCode.RightArrow:
-                return t => t.Translate(gridSize, 0, 0);
-            case KeyCode.UpArrow:
-                return t => t.Translate(0, -gridSize, 0);
-            case KeyCode.DownArrow:
-                return t => t.Translate(0, gridSize, 0);
-            case KeyCode.D:
-                return t =>
-                {
-                    Instantiate<GameObject>(t.gameObject);
-                };
-        }
-
-        return null;
-    }
-
-    private Action<Transform> GetAllSelectedKeyAction(KeyCode keyCode)
-    {
-        switch (keyCode)
-        {
-            case KeyCode.F:
+            case KeyCode.E:
                 return t =>
                 {
                     var angles = t.rotation.eulerAngles;
-                    angles.x *= -1;
+                    angles.z -= 90;
                     t.rotation = Quaternion.Euler(angles);
                 };
-            case KeyCode.LeftArrow:
-                return t => t.Translate(-gridSize, 0, 0);
-            case KeyCode.RightArrow:
-                return t => t.Translate(gridSize, 0, 0);
-            case KeyCode.UpArrow:
-                return t => t.Translate(0, -gridSize, 0);
-            case KeyCode.DownArrow:
-                return t => t.Translate(0, gridSize, 0);
-            case KeyCode.D:
+            case KeyCode.Q:
                 return t =>
                 {
-                    Instantiate<GameObject>(t.gameObject);
+                    var angles = t.rotation.eulerAngles;
+                    angles.z += 90;
+                    t.rotation = Quaternion.Euler(angles);
+                };
+            case KeyCode.W:
+                return t =>
+                {
+                    var scale = t.localScale;
+                    scale.x += 1;
+                    t.localScale = scale;
+                };
+            case KeyCode.S:
+                return t =>
+                {
+                    var scale = t.localScale;
+                    scale.x -= 1;
+                    t.localScale = scale;
+                };
+            case KeyCode.LeftArrow:
+                return t => {
+                    t.Translate(-gridSize, 0, 0);
+                    sceneCamera.transform.Translate(-gridSize, 0, 0);
+                };
+            case KeyCode.RightArrow:
+                return t => {
+                    t.Translate(gridSize, 0, 0);
+                    sceneCamera.transform.Translate(gridSize, 0, 0);
+                };
+            case KeyCode.UpArrow:
+                return t => {
+                    t.Translate(0, -gridSize, 0);
+                    sceneCamera.transform.Translate(0, -gridSize, 0);
+                };
+            case KeyCode.DownArrow:
+                return t => {
+                    t.Translate(0, gridSize, 0);
+                    sceneCamera.transform.Translate(0, gridSize, 0);
+                };
+            case KeyCode.D:
+                return t => {
+                    var newObj = Instantiate<GameObject>(t.gameObject);
+                    Selection.objects = new GameObject[] { newObj };
                 };
         }
 
