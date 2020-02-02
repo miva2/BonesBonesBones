b167276f47ed7dd0ecbe753e1db71396abb349f1
Jesse Gielen <jessegielen@hotmail.com>
2020-02-02 15:41:14 +0000

    Destory bones every x seconds

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,17 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public BonyCharacter player;
     void Start()
     {
-        
+        timeToDamage = boneDestroyInterval;
     }
+
+    public float boneDestroyInterval;
+    float timeToDamage;
 
     // Update is called once per frame
     void Update()
     {
+        timeToDamage -= Time.deltaTime;
+        if (timeToDamage < 0)
+        {
+            DestoryBone();
+            timeToDamage = boneDestroyInterval;
+        }
+    }
+
+    private void DestoryBone()
+    {
+        var targets = new string[] { "LeftPoint", "RightPoint", "HeadPoint"};
+        var target = targets[UnityEngine.Random.Range(0, 3)];
+        player.TakeDamage(target, 1, null);
     }
 }
