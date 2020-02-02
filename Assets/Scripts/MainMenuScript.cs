59d587712798609132831592535447836d2ae8bc
miva2 <miva2-_-@hotmail.com>
2020-02-02 14:30:58 +0000

    Updated mainmenu

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public Button startButton;
+
+    void Start()
+    {
+        //Calls the TaskOnClick/TaskWithParameters/ButtonClicked method when you click the Button
+        startButton.onClick.AddListener(StartGame);
+       
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+    }
+}
